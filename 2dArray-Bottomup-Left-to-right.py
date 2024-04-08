65dc1f8e6eb038bcc47749fbbe6bdbb23cead91e
Rashmi BS <56731498+Rashmi0986@users.noreply.github.com>
2024-04-08 12:59:57 +0530

    Update 2dArray-Bottomup-Left-to-right.py
    Updated to follow the exact format - easy to recollect

--- 2dArray-Bottomup-Left-to-right.py
+++ 2dArray-Bottomup-Left-to-right.py
@@ -9,18 +9,16 @@ num_rows = len(array_2d)
 num_cols = len(array_2d[0])  # Assuming all rows have the same length
 
 # Traverse the array in reverse order (bottom-up)
-for i in range(num_rows - 1, -1, -1):  # Start from the last row and go backwards
+for i in range(num_rows):  # Start from the last row and go backwards
     for j in range(num_cols - 1, -1, -1):  # Start from the last column and go backwards
         print(array_2d[i][j], end=' ')
     print()  # Print a newline after each row
 
 Result 
-####
-9 8 7 
+#######
+3 2 1 
 6 5 4 
-3 2 1
-
-
+9 8 7 
 
 
 
@@ -36,14 +34,19 @@ num_rows = len(array_2d)
 num_cols = len(array_2d[0])  # Assuming all rows have the same length
 
 # Traverse the array in reverse order (bottom-up)
-for i in range(num_rows):  # Start from the last row and go backwards
+for i in range(num_rows - 1, -1, -1):  # Start from the last row and go backwards
     for j in range(num_cols - 1, -1, -1):  # Start from the last column and go backwards
         print(array_2d[i][j], end=' ')
     print()  # Print a newline after each row
 
 Result 
-#######
-3 2 1 
-6 5 4 
+####
 9 8 7 
+6 5 4 
+3 2 1
+
+
+
+
+
 
