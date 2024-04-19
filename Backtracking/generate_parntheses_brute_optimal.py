d919416c51e62eb7a99504339763a9cb6225faf8
Rashmi BS <56731498+Rashmi0986@users.noreply.github.com>
2024-04-19 16:07:17 +0530

    Update generate_parntheses_brute_optimal.py

--- Backtracking/generate_parntheses_brute_optimal.py
+++ Backtracking/generate_parntheses_brute_optimal.py
@@ -1,5 +1,4 @@
 #Bruteforce code
-
 def generate_valid_parentheses(n):
     def is_valid(s):
         stack = []
@@ -28,10 +27,6 @@ n = 3
 print(generate_valid_parentheses(n))
 
 
-
-
-
-
 #Optimal with backtracking method
 def valid(path, n):
     counts = Counter(path)
@@ -57,3 +52,36 @@ def generate_parentheses(n: int) -> List[str]:
     traverse(0, [])       
     
     return res
+
+
+#Space optimised  valid method 
+SC = O(1), TC = O(N)
+def valid(path, n)
+    for char in path:
+        if char == '(':
+            opencount +=1
+        elif char == ')':
+            closecount -= 1
+        if closecount > opencount:
+            return False
+        if open_count > n:
+                return False
+    return True
+
+def generate_parentheses(n: int) -> List[str]:
+    res = []
+    def traverse(state, path):
+        
+        if state == 2*n:
+            res.append(''.join(path))
+        
+        for bracket in ['(', ')']:
+            path.append(bracket)
+            if valid(path,n):
+                traverse(state+1, path)
+            path.pop()    
+            
+    traverse(0, [])       
+    
+    return res
+
