915924c26ea0358c4b7a3fba39f229c564f9e423
Rashmi BS <56731498+Rashmi0986@users.noreply.github.com>
2024-04-24 12:15:32 +0530

    Update Numberof_islands.py

--- BFS_graphs/Numberof_islands.py
+++ BFS_graphs/Numberof_islands.py
@@ -1,1 +1,21 @@
-  
+def sink_island(r: int, c: int, grid: List[List[int]]) -> List[List[int]]:
+    grid[r][c] = 0
+
+    for delta_row, delta_column in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+        neighbor_row = r + delta_row
+        neighbor_column = c + delta_column
+        if 0 <= neighbor_row < len(grid) and 0 <= neighbor_column < len(grid[0]):
+            if grid[neighbor_row][neighbor_column] == 1:
+                sink_island(neighbor_row, neighbor_column, grid)
+
+    return grid
+
+
+def count_number_of_islands(grid: List[List[int]]) -> int:
+    res = 0
+    for r in range(len(grid)):
+        for c in range(len(grid[0])):
+            if grid[r][c] == 1:
+                sink_island(r, c, grid)
+                res += 1
+    return res  
