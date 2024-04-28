7f0fddb2d531a40fd39fe3743172c8340335839f
Rashmi BS <56731498+Rashmi0986@users.noreply.github.com>
2024-04-28 12:33:43 +0530

    Update and rename 2dArray-Bottomup-Left-to-right.py to 2dArray-Bottomup-right-to-left.py

--- 2dArray-Bottomup-right-to-left.py
+++ 2dArray-Bottomup-right-to-left.py
@@ -3,13 +3,14 @@ array_2d = [
     [4, 5, 6],
     [7, 8, 9]
 ]
+#Note: row remains constant but column decrements backwards thats how the for loop designed. 
 
 # Get the dimensions of the array
 num_rows = len(array_2d)
 num_cols = len(array_2d[0])  # Assuming all rows have the same length
 
 # Traverse the array in reverse order (bottom-up)
-for i in range(num_rows):  # Start from the last row and go backwards
+for i in range(num_rows):  # Start from the 0th row .
     for j in range(num_cols - 1, -1, -1):  # Start from the last column and go backwards
         print(array_2d[i][j], end=' ')
     print()  # Print a newline after each row
@@ -22,7 +23,8 @@ Result
 
 
 
-
+#row-wise reverse traversal (bottom-up)
+#Notice rows and columns both decrements thats how for loop designed to pick the elements
 array_2d = [
     [1, 2, 3],
     [4, 5, 6],
