7f199d0425da0ffa5f97907efd152e19b8017db3
Rashmi BS <56731498+Rashmi0986@users.noreply.github.com>
2024-06-25 11:23:34 +0530

    Update Generate_valid_Ip_backtrack.py

--- Backtracking/Generate_valid_Ip_backtrack.py
+++ Backtracking/Generate_valid_Ip_backtrack.py
@@ -1,3 +1,23 @@
+#Brute force approach 
+class Solution:
+    #ref link - https://leetcode.com/problems/restore-ip-addresses/solutions/3079093/python3-simple-brute-force-solution/
+    def works(self, s):
+        return s == str(int(s)) and int(s) <= 255 and int(s) >= 0
+
+    def restoreIpAddresses(self, s: str) -> List[str]:
+        n = len(s)
+        ans = []
+        for i in range(1, n):
+            for j in range(i + 1, n):
+                for k in range(j + 1, n):
+                    a, b, c, d = s[:i], s[i:j], s[j:k], s[k:]
+                    if self.works(a) and self.works(b) and self.works(c) and self.works(d):
+                        ans.append(f'{a}.{b}.{c}.{d}')
+        return ans
+
+
+#backtracking
+
 #https://iq.opengenus.org/generating-ip-addresses/
 
 class Solution:
