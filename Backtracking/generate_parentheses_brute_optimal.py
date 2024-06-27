7fdee2d14e3505e9755a2e5fc104cad4e3e55bd7
Rashmi BS <56731498+Rashmi0986@users.noreply.github.com>
2024-06-27 07:06:38 +0530

    Update generate_parentheses_brute_optimal.py
    Re-organised

--- Backtracking/generate_parentheses_brute_optimal.py
+++ Backtracking/generate_parentheses_brute_optimal.py
@@ -6,6 +6,33 @@ Contraints : while generating the parenthese
             Learn't from this video = https://www.youtube.com/watch?v=s9fokUqJ76A
 """
 
+
+#Optimal with backtracking method
+def valid(path, n):
+    counts = Counter(path)
+    if counts['('] > n or counts[')'] > n or counts[')'] > counts['(']:
+        return False
+    return True
+
+def generate_parentheses(n: int) -> List[str]:
+
+    res = []
+    
+    def traverse(state, path):
+        
+        if state == 2*n:
+            res.append(''.join(path))
+        
+        for bracket in ['(', ')']:
+            path.append(bracket)
+            if valid(path,n):
+                traverse(state+1, path)
+            path.pop()    
+            
+    traverse(0, [])       
+    
+    return res
+
 #Bruteforce code
 def generate_valid_parentheses(n):
     def is_valid(s):
@@ -34,32 +61,6 @@ def generate_valid_parentheses(n):
 n = 3
 print(generate_valid_parentheses(n))
 
-
-#Optimal with backtracking method
-def valid(path, n):
-    counts = Counter(path)
-    if counts['('] > n or counts[')'] > n or counts[')'] > counts['(']:
-        return False
-    return True
-
-def generate_parentheses(n: int) -> List[str]:
-
-    res = []
-    
-    def traverse(state, path):
-        
-        if state == 2*n:
-            res.append(''.join(path))
-        
-        for bracket in ['(', ')']:
-            path.append(bracket)
-            if valid(path,n):
-                traverse(state+1, path)
-            path.pop()    
-            
-    traverse(0, [])       
-    
-    return res
 
 
 #Space optimised  valid method 
